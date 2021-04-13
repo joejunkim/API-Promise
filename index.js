fe5fe3fb57564a70684c5150b94c300ce36a77d1
arath1869 <arathvidrio@gmail.com>
2021-04-13 14:46:06 -0700

    phase 3 almost done

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const fetch = require('node-fetch');
+let personObj = undefined;
 
-fetch('https://swapi.dev/api/people/1/')
+    fetch('https://swapi.dev/api/people/1/')
     .then((response) => {
         return response.json()
     })
     .then(person => {
+        personObj = person
+        console.log(person.name)
         const homeworld = person.homeworld;
-        const fetchPlanet = fetch(homeworld).then(response => response.json());
-        return fetchPlanet;
+        return fetch(homeworld).then(response => response.json())
     })
     .then(planet => {
-        console.log(planet.name);
+        console.log(planet.name)
+        const films = personObj.films
+        return films
+    }).then(films => {
+        let filmArray = films.map(film => {
+            return fetch(film).then(response => response.json())
+        })
+        
     })
+    
